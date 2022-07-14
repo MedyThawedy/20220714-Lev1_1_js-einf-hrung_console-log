234044e19f9426fc99e0551a55447bf95cc3c284
MD <dhmedy@googlemail.com>
2022-07-14 15:53:41 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+document.write(b);
+console.log(document.URL);
+
+console.log("Anton");
+let fn = 'Anton';
+console.log(fn);
+let age = 28;
+console.log(age)
+let job = 'Trainer';
+console.log(job)
+let married = true;
+console.log(married)
+
+console.log(fn + ' ist ' + age + ' Jahre alt');
